e11ad4270effdb23ea14ca9172d2593e3e33ad60
Tim Taylor <ttaylor@systemsup.co.uk>
2016-11-08 18:19:47 +0000

    Break out PackageManagement to separate Artifact

--- Artifacts/Windows-PackageManagement/install.cmd
+++ Artifacts/Windows-PackageManagement/install.cmd
@@ -1,25 +1,14 @@
 @echo OFF
 setlocal
 
-rem https://www.microsoft.com/en-us/download/details.aspx?id=40855
-rem three installers. x32 client, x64 client (but not w8, which has same build/ver as server 2012), x64 server.
-rem 
-Rem Windows 7 SP1 [6.1][7600 or 7601]
-rem  x64: Windows6.1-KB2819745-x64-MultiPkg.msu
-rem  x86: Windows6.1-KB2819745-x86.msu
-rem Windows Server 2008 R2 SP1 [6.1][7600 or 7601]
-rem  x64: Windows6.1-KB2819745-x64-MultiPkg.msu
-rem Windows 8 [6.2][9200]
-rem  NOT SUPPORTED. Upgrade to 8.1
-rem Windows Server 2012 [6.2][9200]
-rem  x64: Windows8-RT-KB2799888-x64.msu
-
-rem if 6.1
-rem  if x86 Windows6.1-KB2819745-x86.msu
-rem  if x64 Windows6.1-KB2819745-x64-MultiPkg.msu
-rem if 6.2
-rem  if server Windows8-RT-KB2799888-x64.msu
-rem  if client NOT SUPPORTED
+rem PowerShell PackageManagement installation 
+rem https://www.microsoft.com/en-us/download/details.aspx?id=51451
+rem 6.3 9600 Windows Server 2012 R2
+rem 6.3 9600 Windows 8.1 Pro
+rem 6.3 9600 Windows 8.1 Enterprise
+rem 6.2 9200 Windows Server 2012
+rem 6.1 7601 Windows 7 SP1
+rem 6.1 7601 Windows Server 2008 R2 SP1
 
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OSVERSION=32BIT || set OSVERSION=64BIT
 
@@ -36,19 +25,27 @@ echo Target Type    : %TARGETTYPE%
 
 if %CURRENTVERSION%==6.1 (
  if %OSVERSION%==32BIT (
-  call Windows6.1-KB2819745-x86.msu /quiet /norestart
+  call PackageManagement_x86.msi /quiet /norestart
  ) else (
-  call Windows6.1-KB2819745-x64-MultiPkg.msu /quiet /norestart
+  call PackageManagement_x64.msi /quiet /norestart
  )
 ) else (
  if %CURRENTVERSION%==6.2 (
   if %TARGETTYPE%==SERVER (
-   call Windows8-RT-KB2799888-x64.msu /quiet /norestart
+   call PackageManagement_x64.msi /quiet /norestart
   ) else (
    echo Not Supported on Windows 8. Please upgrade to 8.1
   )
  ) else (
-  echo Not a supported OS. Windows 7, 2008 R2 or 2012 only.
+  if %CURRENTVERSION%==6.3 (
+	 if %OSVERSION%==32BIT (
+	  call PackageManagement_x86.msi /quiet /norestart
+	 ) else (
+	  call PackageManagement_x64.msi /quiet /norestart
+	 )
+  ) else (
+   echo Not Supported OS. Please check https://www.microsoft.com/en-us/download/details.aspx?id=51451
+  )
  )
 )
 
